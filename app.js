276ce5523108635699f23d22456292d1f41dbc5f
kiyuki <=>
2018-11-08 17:03:22 +0900

    1108_done demo Uty to ndjs

--- app.js
+++ app.js
@@ -1,10 +1,20 @@
 //httpモジュールをインポート
 var http = require('http');
+var dt = require('./myfirstmodule');
+var WebSocketServer = require('ws').Server
+var wss = new WebSocketServer({port:13315});
 
 //Webサーバーの設定
+/*
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.write("The date and time are currently: " + dt.myDateTime());
     res.end('Hello World\n');
-}).listen(3000);
+}).listen(13315);
+*/
 
 console.log('Server running at http://localhost:3000/');
+
+wss.on('connection', function(ws){
+    ws.send('connect')
+});
